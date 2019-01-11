4e2b3a93f6bf39ea07d0cfabfa8d95f08610753b
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-11 02:50:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount, :balance
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? == true && receiver.valid? == true ? true : false
+  end
+
+  def execute_transaction
+    if valid? == true
+      self.new.receiver.deposit(amount)
+      receiver.balance
+
+    else
+      puts "Transaction rejected. Please check your account balance."
+  end
+end
+
 end
